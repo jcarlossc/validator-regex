64e65a92ba85231c3b62c1ef9d78887c1200eaf3
jcarlossc <josecscosta@fac.pe.senac.br>
2025-11-03 19:38:34 -0300

    Teste CNPJ

---
+++ tests/test_cnpjvalidation.py
@@ -0,0 +1,9 @@
+import pytest
+from validator_regex.RegexValidator import RegexValidator
+
+def test_cnpjvalidation_valid():
+    assert RegexValidator.is_cnpj("12.569.987/0211-95")
+
+def test_cnpjvalidation_invalid():
+    assert not RegexValidator.is_cnpj("12345678912345")
+    assert not RegexValidator.is_cnpj("12.3456.789.1234")
